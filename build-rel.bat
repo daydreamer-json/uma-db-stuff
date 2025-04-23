c428c937062e1bc816a0e3770c15cff2f8b0dadc
daydreamer-json <atarakima1sudo@gmail.com>
2025-04-23 11:10:01 +0900

    Add build bat

---
+++ build-rel.bat
@@ -0,0 +1,8 @@
+@echo off&cd /d %~dp0
+mkdir build\uma-db-stuff
+xcopy bin build\uma-db-stuff\bin /E /I /Q
+copy uma-db-stuff.exe build\uma-db-stuff
+cd build
+7z a -tzip -mx=9 uma-db-stuff.zip uma-db-stuff
+cd ..
+rmdir /s /q build\uma-db-stuff
